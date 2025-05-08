2331e7ffe816758c7f3efaf5946bb9c95eda23b4
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-05-08 18:02:50 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Volunteer Portal</h1>
+      <a href="">Log In</a>
+      <a href="">Sign Up</a>
+    </>
+  );
+}
+
+export default App;
