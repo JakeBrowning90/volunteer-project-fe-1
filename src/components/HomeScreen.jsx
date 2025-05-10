631d015f52da095ec762075c0e3b88c01cdfe0da
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-05-10 14:43:35 -0400

    Add placeholder fields to homepage

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -26,6 +26,9 @@ function HomeScreen(
           <p>{localStorage.role}</p>
         </div>
       )}
+      <p>TBA: Account details</p>
+      <p>TBA: Activity summary, log of hours</p>
+      <p>TBA: Search for orgs and opportunities</p>
 
       <a onClick={logout}>Log Out</a>
     </div>
