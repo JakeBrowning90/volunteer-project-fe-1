6bbfaf2d914e18e4646980c655034d13e77cbde6
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-05-09 20:41:49 -0400

    Add template compenent file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react";
 import "./App.css";
 
+// Login
+// Signup
+// Dashboard
+// User info
+// Find Orgs
+// Sign up for shift
+// Hours report
+
 function App() {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
+
   return (
     <>
-      <h1>Volunteer Portal</h1>
-      <a href="">Log In</a>
-      <a href="">Sign Up</a>
+      <header></header>
+      <main>
+        <h1>Volunteer Portal</h1>
+        <a href="">Log In</a>
+        <a href="">Sign Up</a>
+      </main>
     </>
   );
 }
---
+++ src/components/xTemplateComponent.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+// import apiSource
+
+
+function ComponentName({
+    // Props
+}) {
+    // State declarations
+    // Functions
+    // Render
+    return (
+        <div></div>
+    )
+}
+
+// export default ComponentName
