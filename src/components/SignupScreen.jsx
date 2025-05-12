6836c35dc0acc9bb5f690d8e9b00d87b981fdb66
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-05-11 20:08:39 -0400

    Add placeholder style

--- src/components/SignupScreen.jsx
+++ src/components/SignupScreen.jsx
@@ -9,11 +9,60 @@ function SignupScreen(
   }
 ) {
   // State declarations
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [invalidSignup, setInvalidSignup] = useState(false);
+
   // Functions
+  function handleUsername(e) {
+    setUsername(e.target.value);
+  }
+
+  function handlePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleConfirmPassword(e) {
+    setConfirmPassword(e.target.value);
+  }
+
+  async function submitSignup(e) {
+    e.preventDefault();
+  }
   // Render
   return (
     <div>
       <h1>Sign Up</h1>
+      {invalidSignup && <p>Invalid signup</p>}
+      <form onSubmit={submitSignup}>
+        <label htmlFor="usernameInput">Username:</label>
+        <input
+          type="text"
+          name=""
+          id="usernameInput"
+          value={username}
+          onChange={handleUsername}
+        />
+        <label htmlFor="passwordInput">Password:</label>
+        <input
+          type="password"
+          name=""
+          id="passwordInput"
+          value={password}
+          onChange={handlePassword}
+        />
+        <label htmlFor="confirmPasswordInput">Confirm Password:</label>
+        <input
+          type="password"
+          name=""
+          id="confirmPasswordInput"
+          value={confirmPassword}
+          onChange={handleConfirmPassword}
+        />
+        <button>Submit</button>
+      </form>
       <Link to="/login">Back to Login</Link>
     </div>
   );
