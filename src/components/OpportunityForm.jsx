4d69b8caaa4d7287ec0db62e224f19d7b7dd749d
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-05-21 21:11:23 -0400

    Add placeholder event form

--- src/components/OpportunityForm.jsx
+++ src/components/OpportunityForm.jsx
@@ -10,51 +10,67 @@ function OpportunityForm(
   }
 ) {
   // State declarations
-  // const [npoName, setNpoName] = useState("");
-  // const [invalidNpo, setInvalidNpo] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [invalidSubmission, setInvalidSubmission] = useState(false);
+
   // // Functions
   // function handleNpoName(e) {
   //   setNpoName(e.target.value);
   // }
 
-  // async function submitNPO(e) {
-  //   e.preventDefault();
-  //   const response = await fetch(apiSource + "npo", {
-  //     method: "POST",
-  //     mode: "cors",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       nponame: npoName,
-  //       admin: localStorage.id,
-  //     }),
-  //   });
-  //   const npoResponse = await response.json();
-  //   if (Array.isArray(npoResponse.errors)) {
-  //     setInvalidNpo(true);
-  //   } else {
-  //     setInvalidNpo(false);
-  //     // Redirect to login
-  //     window.location.href = "/";
-  //   }
-  // }
+  function handleTitle(e) {
+    setTitle(e.target.value);
+  }
+
+  function handleDescription(e) {
+    setDescription(e.target.value);
+  }
+
+  async function submitOpportunity(e) {
+    e.preventDefault();
+    console.log(title, description);
+    // const response = await fetch(apiSource + "opportunity", {
+    //   method: "POST",
+    //   mode: "cors",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     nponame: npoName,
+    //     admin: localStorage.id,
+    //   }),
+    // });
+    // const npoResponse = await response.json();
+    // if (Array.isArray(npoResponse.errors)) {
+    //   setInvalidNpo(true);
+    // } else {
+    //   setInvalidNpo(false);
+    //   // Redirect to login
+    //   window.location.href = "/";
+    // }
+  }
 
   // Render
   return (
     <div>
+            <Link to="/">Back to Home</Link>
+
       <h1>Create Volunteering Opportunity</h1>
-      {/* {invalidNpo && <p>Invalid NPO</p>}
+      {invalidSubmission && <span>Invalid event</span>}
       <form onSubmit={submitOpportunity}>
-        <label htmlFor="npoName">NPO name:</label>
+        <label htmlFor="title">Title:</label>
+        <input type="text" id="title" value={title} onChange={handleTitle} />
+        <label htmlFor="description">Description:</label>
         <input
           type="text"
-          id="npoName"
-          value={npoName}
-          onChange={handleNpoName}
-        /> */}
-        {/* <button>Submit</button>
-      </form> */}
+          id="description"
+          value={description}
+          onChange={handleDescription}
+        />
+
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
