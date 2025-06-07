dc9e8bdf7c2942b46225609b046b7868c811e193
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-06-07 19:44:31 -0400

    Add basic function to register user for opportunity

--- src/components/OpportunityDetail.jsx
+++ src/components/OpportunityDetail.jsx
@@ -12,6 +12,8 @@ function OpportunityDetail(
   const [opp, setOpp] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [regError, setRegError] = useState(false);
+
 
   // Functions
   const { oppId } = useParams();
@@ -34,6 +36,29 @@ function OpportunityDetail(
       .finally(() => setLoading(false));
   }, []);
 
+  async function submitRegistration(e) {
+    e.preventDefault();
+    console.log("User: " + localStorage.id, "Opp: " + oppId);
+    const response = await fetch(apiSource + `user/regopp/${localStorage.id}`, {
+      method: "PUT",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        oppId: oppId,
+      }),
+    });
+    const regResponse = await response.json();
+    if (Array.isArray(regResponse.errors)) {
+      setRegError(true);
+    } else {
+      setRegError(false);
+      // Redirect to login
+      window.location.href = "/";
+    }
+  }
+
   // Render
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Network error, please try again later.</p>;
@@ -50,14 +75,14 @@ function OpportunityDetail(
       <h2>{opp.npo[0].nponame}</h2>
       <p>{opp.description}</p>
 
-      {localStorage.role == "student" && (<>
-        <span>TBA student user: registration form</span>
-        <form action="">
-          <h2>Join Volunteer List for this Role?</h2>
-          <button>
-            Register
-          </button>
-        </form>
+      {localStorage.role == "student" && (
+        <>
+          <span>TBA student user: registration form</span>
+          <form onSubmit={submitRegistration}>
+            <h2>Join Volunteer List for this Role?</h2>
+            {regError && <p>An error has occurred, please try again later</p>}
+            <button>Register</button>
+          </form>
         </>
       )}
       {localStorage.role == "org_admin" && (
